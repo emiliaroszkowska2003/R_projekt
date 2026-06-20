e0103021100841de541bf2992a4c2ff6f18ea7ba
emiliaroszkowska2003 <117861827+emiliaroszkowska2003@users.noreply.github.com>
2026-06-20 15:24:55 +0200

    Create funkcje.R

---
+++ funkcje.R
@@ -0,0 +1,10 @@
+analizuj_zarobki <- function(dane, stanowisko) {
+  # Tu w przyszłości wpiszemy kod filtrujący dane
+  return(paste("Statystyki dla stanowiska:", stanowisko))
+}
+
+porownaj_doswiadczenie <- function(dane) {
+  # Tu w przyszłości wpiszemy kod grupujący dane
+  return("Tabela porównawcza doświadczenia")
+}
+#test git
